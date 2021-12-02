f41ff5e74287aa81e43e6e48764e055dfa415d4b
zoutigo <zoutigo@gmail.com>
2021-12-02 10:04:44 +0100

    project structure and module-resolver

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Hello from "@components/Hello";
 
 export default function App() {
+  Hello();
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <Text>Hello boy fine</Text>
       <StatusBar style="auto" />
     </View>
   );
